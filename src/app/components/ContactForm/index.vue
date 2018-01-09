bf44fb551d8d09d30898a1c0666e181cb95b9e9b
Johan Martin <martin.johan@johan-martin.com>
2018-01-09 09:07:30 +0200

    minimal config

--- src/app/components/ContactForm/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="./template.html"></template>
-<script lang="ts" src="./contactform.ts"></script>
-<style scoped lang="scss" src="./style.scss"></style>
--- src/app/ts/app.ts
+++ src/app/ts/app.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import * as Webfont from "webfontloader";
-import ContactForm from "../components/ContactForm/ContactForm";
 
 Webfont.load({
   google: {
